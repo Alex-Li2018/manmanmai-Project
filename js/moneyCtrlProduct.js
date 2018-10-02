6188d7d33f2340bf575d9727898cfe0b41bb3f25
Alex-Li2018 <392064881@qq.com>
2018-10-02 17:10:53 +0800

    第五次提交完成省钱控页面

---
+++ js/moneyCtrlProduct.js
@@ -0,0 +1,78 @@
+
+
+$(function(){
+
+	//初始化插件达到弹簧效果
+	var swiper = new Swiper('.swiper-container', {
+	   direction: 'horizontal', //改成横向的变化
+	   slidesPerView: 'auto',
+	   freeMode: true,
+	   scrollbar: {
+	     el: '.swiper-scrollbar',
+	   },
+	   mousewheel: true,
+	});
+
+	var moneyCtrlProduct = {
+		//获取页面url路径里面的productid
+		productId : location.search,
+		//获取图文信息并渲染
+		getPicTxtData : function(){
+
+			// 看是否获取到console.log(this.productId);
+			//处理获取到的id
+			var ID = +this.productId.slice(11); //转成num类型
+			this.sendAjax(ID,renderPicTxt);
+
+			function renderPicTxt (obj) {
+
+				//console.log(obj.result[0]);
+				var objData = obj.result[0];
+				//替换图片
+				$('.pic').html(objData.productImg2);
+				//替换info里面的内容
+				$('.info .mall').text(objData.productFrom);
+				$('.info .addtime').text(objData.productTime);
+				$('.info .author').text(objData.productTips);
+				//更换标题
+				$('.title').text(objData.productName);
+				$('.subtitle').text(objData.productPinkage);
+				//更换内容
+				$('.content .conn').text(objData.productInfo2);
+				//处理获取到的字符串 a链接的字符串 以及描述的字符串
+				var aText = objData.productInfo.slice(0,4);
+				var desText =   objData.productInfo.slice(4);
+				// console.log(aText,desText);
+				$('.content .description a').text(aText);
+				$('.content .description span').text(desText);
+				//添加城市供货列表
+				$('.city').html(objData.productCity);
+				//添加评论	
+				$('.commentCont').html(objData.productComment);
+			}
+		},
+		//封装的发送ajax的方法
+		sendAjax : function(productId,fn){
+			var baseURL = 'http://localhost:9090/api/';
+			$.ajax({
+				url : baseURL + 'getmoneyctrlproduct',
+				dataType : 'json',
+				data : {productid : productId},
+				success : fn
+			});
+		},
+
+		//点击导航栏active切换
+		overturnActive : function(){
+			$('.swiper-slide li').on('click',function(){
+				$(this).addClass('active').siblings().removeClass('active');
+				
+			});
+		}
+	}
+
+	//调用函数渲染页面
+	moneyCtrlProduct.getPicTxtData();
+	//导航栏切换active类的操作
+	moneyCtrlProduct.overturnActive();
+});
---
+++ js/moneyctrl.js
@@ -0,0 +1,111 @@
+
+//入口函数
+$(function(){
+
+	//初始化插件达到弹簧效果
+	var swiper = new Swiper('.swiper-container', {
+	   direction: 'horizontal', //改成横向的变化
+	   slidesPerView: 'auto',
+	   freeMode: true,
+	   scrollbar: {
+	     el: '.swiper-scrollbar',
+	   },
+	   mousewheel: true,
+	});
+
+	//省钱控所有的功能
+	var moneyctrl = {
+		pageNum : null, //记录总页数
+		pageId : 1, //初始化的页面id
+		//发送数据渲染图文混排内容
+		getPicTxtData : function() {
+			
+			sendAjax(this.pageId,renderPicTxtAndPaging);
+			// fn函数事件响应监听函数
+			function renderPicTxtAndPaging (obj) {
+					//console.log(obj);
+					//调用模板渲染图文混排的数据
+					var picTxtHTML = template('tpl-picTxt',obj);
+					$('#main-picTxt').html(picTxtHTML);
+
+					//调用模板渲染下拉框分页的数据
+				 	pageNum = Math.ceil(obj.totalCount / 14);
+					var paging = template('tpl-Paging',{list : pageNum});
+					$('#selectPage').html(paging);
+
+					//因为数据是模板渲染上去的所以对于下拉框的操作也要放在监听函数里面
+					getNextPrevCurrPicTxtData();
+					//发送数据请求当前下一页上一页的数据并渲染
+					function getNextPrevCurrPicTxtData () {
+
+						var  pageid= +$('#selectPage').val();; //pageid的页码数
+						//------下拉框数据改变就发送ajax发送页面页码和分类ID并渲染页面---------
+						$('#selectPage').on('change', function() {
+
+						    //获取选择框里面的id号码,将字符串转成number类型
+						    pageid = +$('#selectPage').val();
+						    //渲染页面
+						    sendAjax(pageid, renderPicTxt);
+						});
+
+						//--------------------上一页与下一页的功能实现----------------------
+						$('.prevPage').on('click',function(){
+							//获取下一页的页码
+							var currentPageId = pageid - 1;
+							if(currentPageId <= 0) { //如果上一页小于0不执行下面的程序
+								return;
+							}
+							pageid = currentPageId;
+
+							//设置下拉框的显示内容是当前的页码数
+							$('#selectPage').val(currentPageId);
+							//渲染数据
+							sendAjax(currentPageId, renderPicTxt);
+						});
+
+						$('.nextPage').on('click',function(){
+
+							var currentPageId = pageid + 1;
+							if(currentPageId >= pageNum) { //如果下一页大于总页数不执行下面的程序
+								return;
+							}
+						    pageid = currentPageId;
+						    $('#selectPage').val(currentPageId);
+						    sendAjax(currentPageId, renderPicTxt);
+						})
+
+						//渲染图文列表的函数
+						function renderPicTxt(obj) {
+							var picTxtHTML = template('tpl-picTxt',obj);
+							$('#main-picTxt').html(picTxtHTML);
+						}
+					}
+			}
+			//发送数据的ajax封装
+			function sendAjax (pageId,fn){
+				var baseURL = 'http://localhost:9090/api/';
+				$.ajax({
+					url : baseURL + 'getmoneyctrl',
+					dataType : 'json',
+					data : {
+						pageid : pageId
+					},
+					success : fn 
+				})
+			}
+		},
+		//点击导航栏active切换
+		overturnActive : function(){
+			$('.swiper-slide li').on('click',function(){
+				$(this).addClass('active').siblings().removeClass('active');
+				
+			});
+		}
+	}	
+
+	//调用方法渲染页面的图文混排的数据
+	moneyctrl.getPicTxtData();
+	//切换active类
+	moneyctrl.overturnActive();
+})
+
